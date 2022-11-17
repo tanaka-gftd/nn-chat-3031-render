58a0b4677c8790b85ca2b58dfb1b71e35e5dc761
Tanaka <f.tanaka@gftd.works>
2022-11-17 11:22:57 +0900

    renderで実行させるために作成

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+const http = require('http');
+const auth = require('http-auth');
+const router = require('./lib/router');
+
+const basic = auth.basic({
+  realm: 'Enter username and password.',
+  file: './users.htpasswd'
+});
+
+const server = http.createServer(basic.check((req, res) => {
+  router.route(req, res);
+})).on('error', (e) => {
+  console.error('Server Error', e);
+}).on('clientError', (e) => {
+  console.error('Client Error', e);
+});
+
+//選択的代入を行うことで、renderかローカル環境下で、使用するポート番号を使い分ける
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.info(`Listening on ${port}`);
+});
